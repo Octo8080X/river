6211cd8c920e036be22465d4d2f710839b47ce83
Octo8080 <okutannderuta0529@gmail.com>
2025-07-09 01:53:17 +0900

    update

--- src/lib.ts
+++ src/lib.ts
@@ -1,29 +1,29 @@
-interface ResSuc<T>{
+interface ResultSuccess<T>{
   v: T;
 }
-interface ResFail<T, F>{
+interface ResultFailure<T, F>{
   v: T;
   e: F;
 }
-type Res<T,F> = ResSuc<T>| ResFail<T,F>;
+type Result<T,F> = ResultSuccess<T>| ResultFailure<T,F>;
 
 // 複数のエラー型を処理できるA関数
 function A<F1 extends string, F2 extends string, F3 extends string>(
-  r1: () => Res<number, F1>,
-  r2: (v: number) => Res<number, F2>,
-  r3: (v: number) => Res<number, F3>
-): ((s: ((res: ResFail<never, F1 | F2 | F3>) => number) | undefined) => number) {
+  r1: () => Result<number, F1>,
+  r2: (v: number) => Result<number, F2>,
+  r3: (v: number) => Result<number, F3>
+): ((s: ((res: ResultFailure<never, F1 | F2 | F3>) => number) | undefined) => number) {
   // FFFFは全てのエラー型の和集合
   type FFFF = F1 | F2 | F3;
-  
-  return (s?: ((res: ResFail<never, FFFF>) => number)): number => {
+
+  return (s?: ((res: ResultFailure<never, FFFF>) => number)): number => {
     const res = r1();
   
     if(res && 'e' in res) {
       // エラーが発生した場合はそのまま返す
       if (s) {
         // s が定義されている場合は、s を呼び出して結果を返す
-        return s({e: res.e, v: undefined as never} as ResFail<never, FFFF>);
+        return s({e: res.e, v: undefined as never} as ResultFailure<never, FFFF>);
       }
       return res.v;
     }
@@ -31,7 +31,7 @@ function A<F1 extends string, F2 extends string, F3 extends string>(
     const res2 = r2(res.v);
     if(res2 && 'e' in res2) {
       if (s) {
-        return s({e: res2.e, v: undefined as never} as ResFail<never, FFFF>);
+        return s({e: res2.e, v: undefined as never} as ResultFailure<never, FFFF>);
       }
       return res2.v;
     }
@@ -39,7 +39,7 @@ function A<F1 extends string, F2 extends string, F3 extends string>(
     const res3 = r3(res2.v);
     if(res3 && 'e' in res3) {
       if (s) {
-        return s({e: res3.e, v: undefined as never} as ResFail<never, FFFF>);
+        return s({e: res3.e, v: undefined as never} as ResultFailure<never, FFFF>);
       }
       return res3.v;
     }
@@ -49,9 +49,9 @@ function A<F1 extends string, F2 extends string, F3 extends string>(
 
 }
 
-const r1 = (): Res<number, "EEE1"> => ({v: 1});
-const r2 = (s:number): Res<number, "EEE2"> => ({v: s + 1});
-const r3 = (s:number): Res<number, "EEE3"> => ({v: s + 2});
+const r1 = (): Result<number, "EEE1"> => ({v: 1});
+const r2 = (s:number): Result<number, "EEE2"> => ({v: s + 1});
+const r3 = (s:number): Result<number, "EEE3"> => ({v: s + 2});
 
 // 型推論によってFFFFは "EEE1" | "EEE2" | "EEE3" になる
 const a = A(r1, r2, r3);
@@ -59,9 +59,9 @@ const aa = a(undefined);
 console.log(aa);
 
 // エラーケースのテスト
-const e1 = (): Res<number, "EEE1"> => ({v: 1, e: "EEE1"});
-const e2 = (s:number): Res<number, "EEE2"> => ({v: s + 1});
-const e3 = (s:number): Res<number, "EEE3"> => ({v: s + 2});
+const e1 = (): Result<number, "EEE1"> => ({v: 1, e: "EEE1"});
+const e2 = (s:number): Result<number, "EEE2"> => ({v: s + 1});
+const e3 = (s:number): Result<number, "EEE3"> => ({v: s + 2});
 
 // エラーハンドラの実装
 //const errorHandler = (res: ResFail<never, "EEE1" | "EEE2" | "EEE3">): number => {
